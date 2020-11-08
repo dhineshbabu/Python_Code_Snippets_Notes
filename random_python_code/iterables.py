4e68c7ce8c7c9d3009981e71ae98931fe9a6e1f3
dhinesh <dhineshbabu89@gmail.com>
2020-11-08 14:46:05 +0530

    added python collections module

---
+++ random_python_code/iterables.py
@@ -0,0 +1,11 @@
+nums = [1,2,3,4]
+
+i_nums = iter(nums)   
+  
+while True:
+    try:
+        item = next(i_nums)
+        print(item)
+    except StopIteration:
+        print("No more data")
+        break
