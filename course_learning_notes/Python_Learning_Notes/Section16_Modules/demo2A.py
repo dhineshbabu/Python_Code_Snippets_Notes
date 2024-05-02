8de22c071cd388fbeee47b7368de1496ce8019ec
Dhineshbabu Muthuraj <dhineshbabumuthuraj@Dhineshbabus-MacBook-Pro.local>
2024-05-01 17:07:52 -0700

    restructured code directory

---
+++ course_learning_notes/Python_Learning_Notes/Section16_Modules/demo2A.py
@@ -0,0 +1,11 @@
+import time
+import demo2
+from imp import reload
+
+demo2.add(10, 20)
+print("Enter into sleeping state")
+time.sleep(40)
+reload(demo2)
+# print("Just wake up... tyring to module again")
+# import demo2 #importing module again after update
+demo2.mul(10, 20)
